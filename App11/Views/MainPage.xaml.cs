eafe314071b476b980ea1c7da439889c96d6704c
Cenk Yenikoylu <mcy@dotnetmcy.net>
2024-07-20 22:32:55 +0300

    settings page footter okay

--- App11/Views/MainPage.xaml.cs
+++ App11/Views/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using App11.ViewModels;
 
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 
 namespace App11.Views;
 
@@ -15,5 +16,60 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+
+        nvSample.SelectedItem = nvSample.MenuItems.OfType<Microsoft.UI.Xaml.Controls.NavigationViewItem>().First();
+
+    }
+
+    private void nvSample_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        try
+        {
+            FrameNavigationOptions navOptions = new FrameNavigationOptions();
+            navOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
+            if (sender.PaneDisplayMode == NavigationViewPaneDisplayMode.Top)
+            {
+                navOptions.IsNavigationStackEnabled = false;
+            }
+            Type pageType = null;
+            if (args.InvokedItem == SamplePage1Item.Content)
+            {
+                pageType = typeof(BlankPage1);
+            }
+            else if (args.InvokedItem == SamplePage2Item.Content)
+            {
+                pageType = typeof(Blank1Page);
+            }
+            else if (args.InvokedItem == SamplePage3Item.Content)
+            {
+                pageType = typeof(SettingsPage);
+            }
+            else if (args.InvokedItem == SamplePage4Item.Content)
+            {
+                pageType = typeof(MainPage);
+            }
+            contentFrame.NavigateToType(pageType, null, navOptions);
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+
+    }
+
+    private void nvSample_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.IsSettingsSelected)
+        {
+            contentFrame.Navigate(typeof(SettingsPage));
+        }
+        else
+        {
+            var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
+            string pageName = "App11.Views." + ((string)selectedItem.Tag);
+            Type pageType = Type.GetType(pageName);
+
+            contentFrame.Navigate(pageType, null, args.RecommendedNavigationTransitionInfo);
+        }
     }
 }
